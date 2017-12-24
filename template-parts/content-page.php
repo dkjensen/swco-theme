0b175a9d926ed69720662c09d93757e34e40c53c
unknown <david@dkjensen.com>
2017-12-24 00:49:04 -0500

    Add meta boxes

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if( ! get_post_meta( get_the_ID(), 'hide_title', true ) ) : ?>
+
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php
